7fc939d6be43126496d9f85701e642c7641a387b
skorlir <skorlir@gmail.com>
2016-01-24 20:06:51 -0600

    Barebones spec code, needs tests

--- src/metablog.js
+++ src/metablog.js
@@ -1,47 +1,89 @@
 // [[METABLOG]]
+var Base = require('_baseclass')
 
-var Classy = require('classy-js')
+var mb = Object.create(null)
+var noop = function () { }
+var mimetypeTest = /[A-z*]+\/[A-z*]+/.test
 
-var Classy$Class$Pluggable = Classy.Module.$Class(function (classyClass) {
-  classyClass.plugins = []
-  classyClass.use = function (plugin) {
-    classyClass.plugins.push(plugin)
-    return classyClass
-  }
-})
+function validMetadata (md) {
+  return (typeof md.title === 'string')
+          ? util.extend2({}, md)
+          : console.warn('Bad metadata:title') && { title: null }
+}
 
-var Pluggable = Classy$Class$Pluggable(Classy);
+function validUri (uri) {
+  return (/(\/?(\/[A-z0-9-_])+)|\//.test(uri))
+          ? uri
+          : console.warn('Bad URI') && '/?!="bad_uri"'
+}
 
-var Render = function (self) {
-  self.render = function () {
-    console.log('render this Renderable')
+function postStatus (post) {
+  var __status = 'draft'
+  post.status = function (s) {
+    if (/^(draft|live)$/.test(s)) __status = s
   }
 }
 
-var Renderable = Pluggable.extend(Render);
+var Collection = mb.Collection = Base.define(function Collection (coll, options) {
+  coll.type  = noop
+  coll.items = [ ]
 
-var Post = Pluggable.extend(function (post) {
-  post.author = ''
-  post.title  = ''
-  post.content = ''
+  Object.getOwnPropertyNames(Array.prototype).forEach(function (f) {
+    if (typeof coll.items[f] === 'function')
+        coll[f] = coll.items[f].bind(coll.items) // push, indexOf, pop, ...
+  })
+
+  util.extend2(coll, options)
 })
 
-var Page = Renderable.extend(function (page) {
-  page.template = 'index.jade'
-})
-
-var Collection = Pluggable.extend(function (col) {
-  col.items = []
-})
-
-var Blog = Pluggable(function (blog) {
-  blog.name = ''
-  blog.url  = 'http'
-})
-
-module.exports = {
-  Blog: Blog,
-  Post: Post,
-  Page: Page,
-  Collection: Collection
+Collection.ofMimetype = function (mt) {
+  if (mimetypeTest(mt))
+    return Collection({ type: function (t) { return mt === t } })
+  else console.error('Cannot make collection of invalid mimetype:', mt)
 }
+
+Collection.of = function (c) {
+  return Collection({ type: c.instanceOf })
+}
+
+var Page = mb.Page = Base.define(function (page, options) {
+  page.metadata = validMetadata(options.metadata)
+  page.uri      = validUri(options.uri)
+  page          = util.extend2(options, page)
+})
+
+var Post = mb.Post = Page.use(postStatus)
+
+var Resource = mb.Resource = Base.define(function (res, options) {
+  res.type = mimetypeTest
+  res.uri  = uri(options.uri)
+})
+
+function Renderer (renderer) {
+  return function Renderable (renderable) {
+    renderable.renderer = renderer.name
+    renderable.render   = renderer.create(renderable)
+  }
+}
+
+var Blog = mb.Blog = Base.define(function Blog (blog, options) {
+  blog.options = util.extend2({}, options)
+
+  blog.Page = Page.use(Renderer(blog.options.pageRenderer || blog.options.renderer))
+  blog.Post = Post.use(Renderer(blog.options.postRenderer || blog.options.renderer))
+
+  ;[ 'Page', 'Post', 'Resource', 'Collection' ].forEach(function (blogPart) {
+    blog[blogPart] = mb[blogPart].use(options[blogPart.toLowerCase() + 's'].use)
+  })
+
+  blog.pages       = Collection.of(blog.Page)
+  blog.posts       = Collection.of(blog.Post)
+  blog.resources   = Collection.of(blog.Resource)
+  blog.collections = Collection.of(blog.Collection)
+
+  blog.add = function (type, thing) {
+    blog[type.toLowerCase() + 's'].push(thing)
+  }
+})
+
+module.exports = mb
