455b186e9688b10a88cc54537ae2411372c6dc86
fluffywaffles <skorlir@gmail.com>
2017-02-03 17:41:39 -0600

    Flush everything. Restart.

--- src/_baseclass.js
+++
@@ -1,7 +0,0 @@
-var Classy = require('classy-js')
-  , IsInstance = require('classy-js/core/is-instance')
-  , Compose = require('classy-js/core/compose')
-  , ToString = require('classy-js/core/to-string')
-  , Equals = require('classy-js/core/equals')
-
-module.exports = Classy().use([ IsInstance, Compose, ToString, Equals ])
--- src/util.js
+++
@@ -1,7 +0,0 @@
-var util = module.exports
-
-util.extend2 = function extend2 (a, b) {
-  for (var bk in b)
-    a[bk] = b[bk]
-  return a
-}
